83bdf678ceb2ca732dfbb38455560550ece7e346
kishanup78910 <79304976+kishanup78910@users.noreply.github.com>
2022-07-12 20:00:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//Define UI varibales
+
+const form =document.querySelector('#task-form');
+const tasklist =document.querySelector('.collection');
+const clearbtn =document.querySelector('.clear-tasks');
+const filter = docuemnt.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+//load All event listener
+loadEventListners();
+
+//Load all event Listners
+function loadEventListners()
+{
+    
+}
